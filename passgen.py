f8552e0d6103f05d3babeabc2bf6ace37797b8e8
Raunak Das <69274839+raunak-dx@users.noreply.github.com>
2020-08-10 11:59:18 +0530

    v1.0

--- passgen.py
+++ passgen.py
@@ -21,9 +21,12 @@ s2 = string.ascii_uppercase
 s3 = string.digits
 #s4 = string.punctuation
 
-l = int(input("Enter password length:\n"))
-n = int(input("Enter no. of passwords to generate:\n"))
-name = input("Enter text file name to store passwords:\n")
+l = int(input("Enter password length: "))
+print("\n")
+n = int(input("Enter no. of passwords to generate: "))
+print("\n")
+name = input("Enter text file name to store passwords: ")
+print("\n")
 name1 = name + ".txt"
 print("Saved passwords in: " + name1)
 print("\n")
@@ -39,4 +42,3 @@ for i in range(1,50):
 	x = open(name1,'a')
 	print("".join(random.sample(s,l)),file = x)
 	x.close()
-
