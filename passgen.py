f6f15c957a6402f3956be627ca07f6e0c3e2165a
Raunak Das <it1811@cemk.ac.in>
2020-08-27 12:42:42 +0530

    v1.0

--- passgen.py
+++ passgen.py
@@ -1,19 +1,17 @@
 import string
 import random
+from colored import fg, bg, attr
 
-class bcolors:
-	OKGREEN = '\033[92m'
-	WARNING = '\033[93m'
-	FAIL = '\033[91m'
-	ENDC = '\033[0m'
+color = fg('green')
+reset = attr('reset')
 
 try:
 	file1 = open('passgen.txt', 'r')
 	print(' ')
-	print (bcolors.OKGREEN + file1.read() + bcolors.ENDC)
+	print (color + file1.read() + reset)
 	file1.close()
 except IOError:
-	print('Banner File not found!\n')
+	print('\nBanner File not found!')
 
 
 s1 = string.ascii_lowercase
